cda808be69b2213d40bd758f1ed90cc5803c0019
Tkahashichika0104 <kk.cc.9225.clover.9480@gmail.com>
2025-11-29 22:43:12 +0900

    kadai_015ファイルを修正

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -2,39 +2,33 @@ package kadai_015;
 
 public class Car_Chapter15 {
     // フィールド
-    private int gear = 1;   // 1速から5速
-    private int speed = 10; // ギアチェンジ後の速度
+    private int gear = 1;   // 現在のギア（初期値 1）
+    private int speed = 10; // 現在の速度（初期値 10）
 
     // ギアを変更するメソッド
     public void changeGear(int afterGear) {
-        // ギアを変更
+        int beforeGear = this.gear; // 変更前ギアを保存
+
+        // ギア変更
         this.gear = afterGear;
 
         // ギアに応じて速度を設定
         switch (gear) {
-            case 1:
-                this.speed = 10;
-                break;
-            case 2:
-                this.speed = 20;
-                break;
-            case 3:
-                this.speed = 30;
-                break;
-            case 4:
-                this.speed = 40;
-                break;
-            case 5:
-                this.speed = 50;
-                break;
-            default:
-                // 上記以外のギア → 時速10km
-                this.speed = 10;
+            case 1:  speed = 10; break;
+            case 2:  speed = 20; break;
+            case 3:  speed = 30; break;
+            case 4:  speed = 40; break;
+            case 5:  speed = 50; break;
+            default: speed = 10;
         }
+
+        // 変更メッセージ表示
+        System.out.println("ギア" + beforeGear + "から" + this.gear + "に切り替えました");
     }
 
     // 走行する（速度を表示する）メソッド
     public void run() {
-        System.out.println("現在の速度は時速" + this.speed + "kmです。");
+        System.out.println("速度は時速" + this.speed + "kmです");
     }
 }
+
