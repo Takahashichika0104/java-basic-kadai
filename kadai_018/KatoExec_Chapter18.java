71331b6cb3f502375e9c5dc27f53eb095f9618a1
Tkahashichika0104 <kk.cc.9225.clover.9480@gmail.com>
2025-11-30 21:30:09 +0900

    kadai_018ファイルを追加

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,23 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+    public static void main(String[] args) {
+
+        // ★ Step1: 各子クラスのインスタンスを作成する
+        KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+        KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+        KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+
+        // ★ Step2: setGivenNameメソッドを実行する
+        taro.setGivenName();
+        ichiro.setGivenName();
+        hanako.setGivenName();
+
+        // ★ Step3: execIntroduceメソッドを実行する
+        taro.execIntroduce();
+        ichiro.execIntroduce();
+        hanako.execIntroduce();
+
+        // ★ end（特に処理はなし）
+    }
+}
